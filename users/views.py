cd29c509e4d893fc231d711bd57cef3baa1c539b
v-rishi <rishi@yodaplus.com>
2022-03-21 15:31:03 +0530

    commit after video-7, authentication, login etc

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -10,8 +11,15 @@ def register(request):
             form.save()
             # lets grab the username for the time being
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username} successfully!')
-            return redirect(to='blog-home')
+            messages.success(request, f'Account created for {username} successfully! Please login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+# @login_required(login_url='login')
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
+    # instead of writing the login_url here, we can also write it in the settings.py file
