d6b450d49a617700ac4e2643cbaeb2891c294950
v-rishi <rishi@yodaplus.com>
2022-03-23 15:28:37 +0530

    commit after video-12: password reset email

--- corey_django_project/settings.py
+++ corey_django_project/settings.py
@@ -131,5 +131,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
-
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
